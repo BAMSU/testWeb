80f86471da450604bf94ae927c11d2406dd61f48
leessipare <leessipare@naver.com>
2017-11-02 11:09:21 +0900

    as

--- src/main/java/member/model/MemberDAO.java
+++ src/main/java/member/model/MemberDAO.java
@@ -1,9 +1,4 @@
 package member.model;
-
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/BAMSU/testWeb.git
 public interface MemberDAO {
 
 	public int Member_Login(String member_id,String member_pwd);
