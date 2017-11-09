147fa71d27738b8b701fd4ae4a096c145ddc7be0
Song <nokcha90@naver.com>
2017-11-09 16:48:55 +0900

    Revert "중간 커밋"
    This reverts commit fe4360fe392c9edb7bfe7473373ec5aa4a5cf9bc.

--- src/main/java/controller/WdController.java
+++ src/main/java/controller/WdController.java
@@ -3,10 +3,7 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.sun.xml.internal.ws.config.management.policy.ManagementPolicyValidator;
 
 import hall.model.HallDAO;
 import hall.model.HallDTO;
@@ -23,23 +20,6 @@ public class WdController {
 		return "wd/insertWd";
 	}
 	
-	@RequestMapping("/insertWd2.we")
-	public ModelAndView adminAction(HallDTO dto) {
-		ModelAndView mav = new ModelAndView();
-		int result = dao.insertWd(dto);
-		
-		if(result>0) {
-			mav.addObject("msg", "등록 성공!");
-			mav.addObject("gourl", "wdList.we");
-			
-		} else {
-			mav.addObject("msg", "등록 실패!");
-			mav.addObject("gourl", "wdList.we");
-		}
-		mav.setViewName("wd/wdMsg");
-		return mav;
-	}
-	
 	@RequestMapping("/wdList.we")
 	public ModelAndView wdList() {
 		ModelAndView mav = new ModelAndView();
@@ -49,28 +29,10 @@ public class WdController {
 	}
 	
 	@RequestMapping("/updateWd.we")
-	public ModelAndView wdContent(int idx) {
+	public ModelAndView wdContent(String name) {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("list", dao.getHallInfo(idx));
+		mav.addObject("list", dao.getHallListByName(name));
 		mav.setViewName("wd/updateWd");
-		return mav;
-	}
-	
-	@RequestMapping("/update2.we")
-	public ModelAndView updateWd(HallDTO dto) {
-		
-		ModelAndView mav = new ModelAndView();
-		int result = dao.updateWd(dto);
-		
-		if(result>0) {
-			mav.addObject("msg", "수정 성공!");
-			mav.addObject("gourl", "wdList.we");
-			
-		} else {
-			mav.addObject("msg", "수정 실패!");
-			mav.addObject("gourl", "wdList.we");
-		}
-		mav.setViewName("wd/wdMsg");
 		return mav;
 	}
 	
--- src/main/java/hall/model/HallDAO.java
+++ src/main/java/hall/model/HallDAO.java
@@ -7,6 +7,4 @@ public interface HallDAO {
 	public List getHallList();
 	public List getHallListByGu(String gu);
 	public List getHallListByName(String name);
-	public int insertWd(HallDTO dto);
-	public int updateWd(HallDTO dto);
 }
