362f9ea52f2d4e0a1659d788628375a8ccf81e5a
Song <nokcha90@naver.com>
2017-11-12 00:12:09 +0900

    ㅇ

---
+++ src/main/java/ahall/model/aHallDAO.java
@@ -0,0 +1,14 @@
+package ahall.model;
+
+import java.util.List;
+
+public interface aHallDAO {
+	public aHallDTO getHallInfo(int idx);
+	public List getHallList();
+	public List getHallGu();
+	public List getHallListByGu(String gu);
+	public List getHallListByName(String name);
+	public int insertWd(aHallDTO dto);
+	public int updateWd(aHallDTO dto);
+	public int deleteWd(int idx);
+}
