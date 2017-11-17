f31443c80ad13d994874ef896ca838e0c72646ee
HWANG <HWANG@DESKTOP-RN6S0AD>
2017-11-17 09:48:39 +0900

    홀 누적 총점수 추가 / 문의하기 css/ sns연동 로그인 추가작업

--- src/main/java/com/sns/login/LoginController.java
+++ src/main/java/com/sns/login/LoginController.java
@@ -41,7 +41,7 @@ public class LoginController {
 	}
 
 	//로그인 첫 화면 요청 메소드
-	@RequestMapping(value = "/login.we", method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(value = "/memberLogin.we", method = { RequestMethod.GET, RequestMethod.POST })
 	public String login(Model model, HttpSession session) {
 		
 		/* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */
@@ -55,7 +55,7 @@ public class LoginController {
 		model.addAttribute("url", naverAuthUrl);
 
 		/* 생성한 인증 URL을 View로 전달 */
-		return "login";
+		return "member/memberLogin";
 	}
 
 	//네이버 로그인 성공시 callback호출 메소드
